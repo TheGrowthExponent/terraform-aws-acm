610b8d2f24e74a3f391982db58de8a83b2933904
Jason Pascoe <jpascoe@growthexponent.com>
2025-03-23 17:40:26 +0800

    Add variable description

--- variables.tf
+++ variables.tf
@@ -16,5 +16,14 @@ variable "environment" {
   }
 }
 
-variable "hosted_zone_id" {}
-variable "host_name" {}
+variable "hosted_zone_id" {
+  description = "The hosted zone id for the domain."
+  type        = string
+  default     = "Z0000000000000000000"
+}
+
+variable "host_name" {
+  description = "The host name for the application."
+  type        = string
+  default     = "example.com"
+}
